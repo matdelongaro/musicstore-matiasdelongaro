bdc66e300d8ea47902bf614d0832ed77582ac63e
matias delongaro <matiasadelongaro@gmail.com>
2024-04-03 12:27:28 -0300

    styles

--- js/bag.js
+++ js/bag.js
@@ -41,7 +41,7 @@ const llamarBag = () => {
     const total = carrito.reduce((acc, el) => acc + el.price, 0);
     const totales = document.createElement("div")
     totales.className = "total-content"
-    totales.innerHTML= `total a pagar: ${total} $`;
+    totales.innerHTML= `Total a pagar: ${total} $`;
     modalContenedor.append(totales);
 };
 
